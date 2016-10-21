2b90fe2dba9bcffb7f0915998cb3478d9d8767e1
Gina <gdebell@gmail.com>
2016-10-21 11:33:38 -0600

    The items for each coffee product in the category display on the drop down select bar.  Next step, create create a filter so the user can select a coffee based on the category words.

--- js/components/stock/stock.controller.js
+++ js/components/stock/stock.controller.js
@@ -2,7 +2,8 @@
   'use strict';
   angular
     .module('CartApp.controller.stock', [])
-    .controller('StockController', StockController);
+    .controller('StockController', StockController)
+    .controller('CategoriesController', CategoriesController);
 
   StockController.$inject = ['StockService'];
 
@@ -10,6 +11,20 @@
     /*jshint validthis: true */
     this.name = 'yo';
     this.productsArray = StockService.productsArray;
+  }
+
+  function CategoriesController (StockService) {
+    var itemList = StockService.productsArray;
+    let categoryList = [];
+
+    itemList.forEach(function(item){
+        for (var i= 0; i < item.categories.length; i++) {
+          if (categoryList.indexOf(item.categories[i]) === -1) {
+            categoryList.push(item.categories[i])
+          }
+        }
+    });
+    this.categoriesArray = categoryList;
   }
 
 })();
